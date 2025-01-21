6bcaaa75ceb2d157cee8db0599eec2e1955b6e2e
Amr Hussein <amrwael6j@gmail.com>
2025-01-21 20:29:04 +0700

    Created the User class

---
+++ RestuarantGit/Models/Domain/User.cs
@@ -0,0 +1,13 @@
+﻿using Delivery.Resutruant.API.Models.Enums;
+using Microsoft.AspNetCore.Identity;
+
+namespace Delivery.Resutruant.API.Models.Domain
+{
+    public class User : IdentityUser
+    {
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
---
+++ RestuarantGit/Models/Enum/Gender.cs
@@ -0,0 +1,8 @@
+﻿namespace Delivery.Resutruant.API.Models.Enums
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
