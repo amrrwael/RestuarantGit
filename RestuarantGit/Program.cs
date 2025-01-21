6858003ae212a296b8fc790233195da5bc31c45a
Amr Hussein <amrwael6j@gmail.com>
2025-01-21 20:41:44 +0700

    Commit: feat: Set up CORS policy for the application
    Description:
    - Added a CORS policy in Program.cs to allow requests from all origins, methods, and headers.
    - Enabled the CORS middleware in the request pipeline to apply the CORS policy globally.

--- RestuarantGit/Program.cs
+++ RestuarantGit/Program.cs
@@ -18,8 +18,16 @@ namespace RestuarantGit
             
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                               .AllowAnyMethod()
+                               .AllowAnyHeader();
+                    });
+            });
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
