3057d9989bc44712d6920a38d3acf6697745d651
Amr Hussein <amrwael6j@gmail.com>
2025-01-21 20:11:08 +0700

    Commit: git commit -m "chore: Create project structure"
    Description:
    - Created a new ASP.NET Core Web API project.
    - Organized the project into folders: Models, Controllers, Services, Repositories, Configurations, Middleware, Migrations, and DataBase.

--- RestuarantGit/Program.cs
+++ RestuarantGit/Program.cs
@@ -6,7 +6,7 @@ namespace RestuarantGit
         public static void Main(string[] args)
         {
 
-            //asfqw21323133123
+            
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
