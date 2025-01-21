6d8e7e8568a82426f374b1879f8b77bea9e00703
Amr Hussein <amrwael6j@gmail.com>
2025-01-21 20:34:19 +0700

    Commit: feat: Set up JWT authentication for the application
    Description:
    - Added the Microsoft.AspNetCore.Authentication.JwtBearer package to enable JWT-based authentication.
    - Created the JwtSettings class to manage JWT configuration options.
    - Added JWT settings (e.g., issuer, audience, secret key) to appsettings.json for secure configuration.
    - Configured JWT authentication middleware in Program.cs to enable token validation and authentication.

---
+++ RestuarantGit/Middleware/JwtMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Delivery.Resutruant.API.Services.Interfaces;
+using System.Net;
+
+namespace Delivery.Resutruant.API.Configrations
+{
+    public class JwtMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IBlacklistService _blacklistService;
+
+        public JwtMiddleware(RequestDelegate next, IBlacklistService blacklistService)
+        {
+            _next = next;
+            _blacklistService = blacklistService;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+            // Check if the token is blacklisted
+            if (token != null && await _blacklistService.IsTokenBlacklistedAsync(token))
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("Unauthorized: Token is blacklisted.");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+
+}
+
+
