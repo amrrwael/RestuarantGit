104a3feb4da7bb0b6a28f19a69082096d8efad8e
Amr Hussein <amrwael6j@gmail.com>
2025-01-22 19:46:44 +0700

    Commit: feat: Implement Dish model, DTO, and creation functionality
    Description:
    - Created the Dish model in the Models/Domain folder to represent dish data in the database.
    - Created the DishDto class in the Models/DTO folder to handle dish data transfer between layers.
    - add new dishes to the database.

---
+++ RestuarantGit/Models/DTO's/DishDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Delivery.Resutruant.API.Models.DTO
+{
+    public class DishDto
+    {
+        public Guid Id { get; set; }
+        public int Price { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Boolean IsVegeterian { get; set; }
+        public string Photo { get; set; }
+        public double Rating { get; set; }
+        public string Category { get; set; } // String representation of enum
+    }
+
+}
---
+++ RestuarantGit/Models/Domain/Dish.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Runtime.Serialization;
+
+namespace Delivery.Resutruant.API.Models.Domain
+{
+    public enum Category
+    {
+        [EnumMember(Value = "WOK")]
+        WOK,
+
+        [EnumMember(Value = "Pizza")]
+        Pizza,
+
+        [EnumMember(Value = "Soup")]
+        Soup,
+
+        [EnumMember(Value = "Desert")]
+        Desert,
+
+        [EnumMember(Value = "Drink")]
+        Drink
+    }
+    public class Dish
+    {
+        public Guid Id { get; set; }
+        public int Price { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Boolean IsVegeterian { get; set; }
+        public string Photo { get; set; }
+        public Category Category { get; set; }
+        public double? Rating{ get; set; }
+
+    }
+}
