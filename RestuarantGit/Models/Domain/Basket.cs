0b3ca9b13d1270d8e1b9a3afc3fbf23778649fa0
Amr Hussein <amrwael6j@gmail.com>
2025-01-24 02:13:30 +0700

    Commit: feat: Create Basket and BasketItem models
    Description:
    - Created the Basket class to represent the basket entity with properties:
      - Id: Unique identifier for the basket.
      - UserEmail: Email of the user associated with the basket.
      - Items: List of BasketItem objects representing items in the basket.
    - Created the BasketItem class to represent items in the basket with properties:
      - Id: Unique identifier for the basket item.
      - DishId: ID of the dish associated with the item.
      - Name: Name of the dish.
      - Price: Price of the dish.
      - TotalPrice: Total price of the item (Price * Amount).
      - Amount: Quantity of the item.
      - Image: Image URL of the dish.
      - BasketId: ID of the basket the item belongs to.

---
+++ RestuarantGit/Models/Domain/Basket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Delivery.Resutruant.API.Models.Domain
+{
+    public class Basket
+    {
+        public Guid Id { get; set; } // Auto-generate Basket ID
+        public string UserEmail { get; set; }
+        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+    }
+
+    public class BasketItem
+    {
+        public Guid Id { get; set; }
+        public Guid DishId { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int TotalPrice { get; set; } // Price * Amount
+        public int Amount { get; set; }
+        public string Image { get; set; }
+        public Guid BasketId { get; set; }
+    }
+
+}
