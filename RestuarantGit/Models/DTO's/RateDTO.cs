b88582cd48f7864a22a5395b75e76bab7d61e516
Amr Hussein <amrwael6j@gmail.com>
2025-01-26 03:27:44 +0700

    Commit: feat: Define Rating model and DTO
    Description:
    - Created the Rating class to represent the rating entity with properties:
      - Id: Unique identifier for the rating.
      - UserEmail: Email of the user who submitted the rating.
      - Value: The rating value (between 1 and 10).
      - DishId: ID of the dish being rated.
      - Dish: Navigation property to the Dish entity.
    - Created the RatingDto class to handle rating data transfer between layers:
      - UserEmail: Email of the user who submitted the rating.
      - Value: The rating value (between 1 and 10).
      - DishId: ID of the dish being rated.

---
+++ RestuarantGit/Models/DTO's/RateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Delivery.Resutruant.API.Models.DTO
+{
+    public class RateDTO
+    {
+        public Guid DishID { get; set; }
+        public double Rate { get; set; }
+    }
+}
---
+++ RestuarantGit/Models/Domain/Rating.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Delivery.Resutruant.API.Models.Domain
+{
+    public class Rating
+    {
+        public Guid Id { get; set; }
+        public string UserEmail { get; set; }
+        [Range(1, 10)]
+        public int Value { get; set; }
+        public Guid DishId { get; set; }
+        public Dish Dish { get; set; }
+    }
+}
